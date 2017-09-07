6bd94001c17d9f051b8f3bc68c73d49bb0711447
jason welsh <jasonbwelsh@gmail.com>
2017-09-07 10:59:51 -0400

    stopped infinite animation loop of name icon. Now ends when brackets scale down and inner bar is blue. Also lessened bottom margin of .name at tablet and above

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
-//fade in header
-const header = document.querySelector(".main-header");
 
+const header = document.querySelector(".main-header");
+const mainNav = document.querySelector(".main-nav");
+const nameIcon = document.querySelector(".name-icon");
+const mainNavList = document.querySelector(".main-nav-list");
+const headerDownArrow = document.querySelector(".header-down-arrow");
+
+//fade in header
 $(document).ready(function() {
 	header.classList.remove("hide");
 	header.classList.add("show");
+});
+
+// hover over name icon to highlight nav links
+nameIcon.addEventListener('mouseover', (e) => {
+	let items = mainNavList.children;
+	for ( let i=0; i < items.length; i += 1 ) {
+		items[i].classList.add("highlight-links");
+	}
+});
+nameIcon.addEventListener('mouseout', () => {
+	let items = mainNavList.children;
+	for ( let i=0; i < items.length; i += 1 ) {
+		items[i].classList.remove("highlight-links");
+	}
+});
+
+// header down arrow removes header and sticks nav
+headerDownArrow.addEventListener('click', () => {
+	mainNav.style.position = "fixed";
+	mainNav.style.top = 0;
+	header.classList.add("shrink-ray");
 });
