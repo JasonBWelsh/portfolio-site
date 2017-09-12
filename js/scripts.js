23222f8bb083787202adeb4a712fb7ac0b835572
jason welsh <jasonbwelsh@gmail.com>
2017-09-12 19:04:23 -0400

    about me about card selection program created. Need to refine now

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,11 @@ const nameIcon = document.querySelector(".name-icon");
 const mainNavList = document.querySelector(".main-nav-list");
 const headerDownArrow = document.querySelector(".header-down-arrow");
 
+const aboutText = document.querySelector(".about-text");
+const aboutList = document.querySelector(".about-list");
+const aboutCards = aboutText.querySelectorAll(".about-card");
+
+
 const skillsList = document.querySelector(".skills-list");
 
 //fade in header
@@ -12,6 +17,44 @@ $(document).ready(function() {
 	header.classList.remove("hide");
 	header.classList.add("show");
 });
+
+
+// set initial .display class on about-coding
+for (let i = 0; i < aboutCards.length; i += 1) {
+	if (aboutCards[i].classList.contains("about-coding")) {
+		aboutCards[i].classList.add("display");
+	}
+}
+
+
+// click about list to show about card
+aboutList.addEventListener('click', (e) => {
+	if (e.target.tagName === 'LI') {
+		let LI = e.target;
+		// loop over about-cards and remove .display class
+		for ( let i = 0; i < aboutCards.length; i += 1) {
+			if (aboutCards[i].classList.contains("display")) {
+				aboutCards[i].classList.remove("display");
+			}
+		}
+		// check LI type and show proper card
+		// check for coding
+		let lis = aboutList.children;
+		if (LI.textContent === 'Coding') {
+			aboutCards[0].classList.add("display");
+		} else if (LI.textContent === 'Health') {
+			aboutCards[1].classList.add("display");
+		} else if (LI.textContent === 'Puppies') {
+			aboutCards[2].classList.add("display");
+		} else if (LI.textContent === 'Music') {
+			aboutCards[3].classList.add("display");
+		} else if (LI.textContent === 'Other') {
+			aboutCards[4].classList.add("display");
+		}
+	}
+});
+
+
 
 // set random skills list item change
 function highlightSkill() {
